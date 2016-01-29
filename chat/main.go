61b595f67c1a4f948e0ba6334a1f3905858e31d8
matsu-chara <matsuy00@gmail.com>
2016-01-29 23:55:30 +0900

    chapter2

--- chat/main.go
+++ chat/main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 
 	"github.com/matsu-chara/goblueprints/chat/trace"
 )
@@ -25,11 +26,18 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
 
 func main() {
-	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
+	addr := flag.String("host", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 	gomniauth.SetSecurityKey("moyomi")
 	gomniauth.WithProviders(
--- chat/room.go
+++ chat/room.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"log"
+
 	"net/http"
+
+	"github.com/stretchr/objx"
 
 	"github.com/gorilla/websocket"
 	"github.com/matsu-chara/goblueprints/chat/trace"
 )
 
 type room struct {
-	forward chan []byte
+	forward chan *message
 	join    chan *client
 	leave   chan *client
 	clients map[*client]bool
@@ -18,7 +21,7 @@ type room struct {
 
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -37,7 +40,7 @@ func (r *room) run() {
 			r.tracer.Trace("クライアントが退室しました")
 			close(client.send)
 		case msg := <-r.forward:
-			r.tracer.Trace("メッセージを受信しました: ", string(msg))
+			r.tracer.Trace("メッセージを受信しました: ", msg.Message)
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
@@ -65,10 +68,17 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("ServeHTTP:", err)
 		return
 	}
+
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("クッキーの取得に失敗しました:", err)
+		return
+	}
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
