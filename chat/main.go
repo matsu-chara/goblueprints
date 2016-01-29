66d0e62f681e7b3b75b361dcfc52282e9ba1b473
matsu-chara <matsuy00@gmail.com>
2016-01-29 23:25:48 +0900

    google oauth

--- chat/main.go
+++ chat/main.go
@@ -9,6 +9,9 @@ import (
 	"sync"
 	"text/template"
 
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+
 	"github.com/matsu-chara/goblueprints/chat/trace"
 )
 
@@ -28,6 +31,10 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+	gomniauth.SetSecurityKey("moyomi")
+	gomniauth.WithProviders(
+		google.New("744730650722-d59t9ljuu4kne10q7qr6fje6f5i27gmo.apps.googleusercontent.com", "79kS5-Ujkwp_5G4gxwlUmGZ4", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
