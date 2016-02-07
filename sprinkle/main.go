058294d1fef7e771093e45106fcb94c96f9dc7f4
matsu-chara <matsuy00@gmail.com>
2016-02-07 18:31:52 +0900

    練習問題

--- sprinkle/main.go
+++ sprinkle/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"os"
@@ -11,24 +12,38 @@ import (
 
 const otherWord = "*"
 
-var transforms = []string{
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord,
-	otherWord + "app",
-	otherWord + "site",
-	otherWord + "time",
-	"get" + otherWord,
-	"go" + otherWord,
-	"lets" + otherWord,
+type transforms struct {
+	Words []string `json:"words"`
+}
+
+func (t transforms) pickWord(r *rand.Rand) string {
+	return t.Words[r.Intn(len(t.Words))]
+}
+
+func readTransforms(filename string) (transforms, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return transforms{}, err
+	}
+	decoder := json.NewDecoder(file)
+
+	var t transforms
+	err2 := decoder.Decode(&t)
+	if err2 != nil {
+		return transforms{}, err
+	}
+	return t, nil
 }
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
+	t, err := readTransforms("transforms.json")
+	if err != nil {
+		fmt.Printf("read transforms error: %v", err)
+	}
+	r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		t := transforms[rand.Intn(len(transforms))]
-		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+		fmt.Println(strings.Replace(t.pickWord(r), otherWord, s.Text(), -1))
 	}
 }
