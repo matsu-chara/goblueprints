8342be18fa933a3e671a74f7027ae79fbd1ba340
matsu-chara <matsuy00@gmail.com>
2016-02-12 01:41:09 +0900

    とりあえずおすすめ

---
+++ chapter7/meander/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/matsu-chara/goblueprints/chapter7/meander"
+)
+
+func main() {
+	//meander.APIKey = "TODO"
+	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+		respond(w, r, meander.Journeys)
+	})
+	http.ListenAndServe(":8080", http.DefaultServeMux)
+}
+
+func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
+	publicData := make([]interface{}, len(data))
+	for i, d := range data {
+		publicData[i] = meander.Public(d)
+	}
+	return json.NewEncoder(w).Encode(publicData)
+}
