0fcfd571e1cde904db06e4f36565aa6f110849fc
matsu-chara <matsuy00@gmail.com>
2016-02-12 12:46:33 +0900

    APIKEY追加

--- chapter7/meander/cmd/main.go
+++ chapter7/meander/cmd/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/matsu-chara/goblueprints/chapter7/meander"
 )
 
 func main() {
-	//meander.APIKey = "TODO"
+	key := os.Getenv("PLACE_API_KEY")
+	if key == "" {
+		log.Fatalln("PLACE_API_KEYが環境変数から取得できませんでした")
+		return
+	}
+	meander.APIKey = key
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
 	})
